61912438a63e58be3dd4fc485a2b967caec346c9
Davis <davissong01@gmail.com>
2022-01-02 17:30:31 -0800

    Clickable recipe-item cards that route to recipe-detail

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeBinded: Recipe;
+  @Input() index: number;
 
   constructor() { }
 
