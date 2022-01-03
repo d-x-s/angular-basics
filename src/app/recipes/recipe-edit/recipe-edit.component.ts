3258019969d48db6fb51909c4b0cbf41f62b2201
Davis <davissong01@gmail.com>
2022-01-02 17:47:43 -0800

    Setup recipe-edit ActivatedRoute connection

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number
+  isEditable = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+
+        // check if this params has an id
+        // return TRUE: we are in edit mode (has id)
+        // return FALSE: we are in new mode (does not have id)
+        this.isEditable = params['id'] != null;
+      }
+    )
   }
 
 }
